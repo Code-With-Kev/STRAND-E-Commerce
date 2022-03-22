e422e2c0c15b02d0c5213bca3a57fe61c9a19b24
Code-With-Kev <96272858+Code-With-Kev@users.noreply.github.com>
2022-03-21 23:34:40 -0700

    Co-authored-by: Kevni Woodside <Code-With-Kev@users.noreply.github.com>

--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -1,36 +1,57 @@
 import { ArrowLeftOutlined, ArrowRightOutlined } from "@mui/icons-material"
+import { useState } from "react";
+import { slideshowSets } from "../javascript/Slideshow";
 
 const Slideshow = () => {
+    const [slideIndex, setSlideIndex] = useState(0);
+
+    const handleClick = position => {
+        if(position === "left") {
+            setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2)
+        } else {
+            setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0)
+        }
+
+    }
+
     return (
         <div className="slideshow">
 
-            <div className="slider--arrow slider--arrow__left">
+            {/* --------------------------------------SLIDE ARROWS--------------------------------------------------- */}
+
+            <div className="slider--arrow slider--arrow__left" onClick={    (e) =>  handleClick("left")     }>
                 <ArrowLeftOutlined className="slider--arrow-icon" />
             </div>
 
-            <div className="slider--arrow slider--arrow__right">
+            <div className="slider--arrow slider--arrow__right" onClick={    (e) =>  handleClick("right")     }>
                 <ArrowRightOutlined className="slider--arrow-icon"/>
             </div>
-    
-            <div className="slideshow--image"></div>
-            
-            <div className="slideshow--info">
-                <div>
-                    <span className="title title__standard">HOW TO</span> <br/>
-                    <div className="skew"><span className="title title__highlight">PROTECT</span><br/></div>
-                </div>
-                <div className="margin-top">
-                    <div className="skew"><span className="title title__highlight">YOUR HAIR</span><br/></div>
-                    <span className="title title__standard">WHILE</span> <br/>
-                    <span className="title title__standard">WEARING A</span> <br/>
-                    <div className="skew"><span className="title title__highlight">HIJAB</span><br/></div>
-                </div>
-    
-                <div className="slider__info-bottom">
-                    <p className="description description__slide" >Find out what materials work best with your natural hair and check out some of our favorite brands</p>
-                    <a href="#" class="splitColor">VISIT &#8594;</a>
+
+            {/* ---------------------------------------SLIDESHOW--------------------------------------------------- */}
+            {slideshowSets.map(slide => 
+
+            <div className="slide">
+                <div className="slide--image" style={{backgroundImage: `url(${slide.img})`}}></div>
+                
+                <div className="slide--info">
+                    <div>
+                        <span className="title title__standard">{slide.standardA}</span> <br/>
+                        <div className="skew"><span className="title title__highlight">{slide.highlightA}</span><br/></div>
+                    </div>
+                    <div className="margin-top">
+                        <div className="skew"><span className="title title__highlight">{slide.highlightB}</span><br/></div>
+                        <span className="title title__standard">{slide.standardB}</span> <br/>
+                        <span className="title title__standard">{slide.standardC}</span> <br/>
+                        <div className="skew"><span className="title title__highlight">{slide.highlightC}</span><br/></div>
+                    </div>
+        
+                    <div className="slider__info-bottom">
+                        <p className="description description__slide">{slide.description}</p>
+                        <a href="#" class="splitColor">VISIT &#8594;</a>
+                    </div>
                 </div>
             </div>
+        )}
         </div>
     )
 }
---
+++ src/javascript/Slideshow.js
@@ -0,0 +1,38 @@
+export const slideshowSets = [
+    {
+        id: 1,
+        img: "https://images.pexels.com/photos/6700513/pexels-photo-6700513.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
+        alt: "Woman in hijab",
+        standardA: "HOW TO",
+        highlightA: "PROTECT",
+        highlightB: "YOUR HAIR",
+        standardB: "WHILE",
+        standardC: "WEARING A",
+        highlightC: "HIJAB",
+        description: "Find out what materials work best with your natural hair and check out some of our favorite brands"
+    },
+    {
+        id: 2,
+        img: "https://www.pexels.com/photo/crop-shirtless-black-man-holding-coffee-cup-5301669/",
+        alt: "Trans person",
+        standardA: "BEING",
+        highlightA: "TRANS",
+        highlightB: "AND PROUD",
+        standardB: "IN A WORLD",
+        standardC: "WITH",
+        highlightC: "TRANSPHOBES",
+        description: "Find out what materials work best with your natural hair and check out some of our favorite brands"
+    },
+    {
+        id: 3,
+        img: "https://www.pexels.com/photo/a-woman-posing-at-the-camera-3888212//",
+        alt: "Women modelling",
+        standardA: "STAYING YOUR",
+        highlightA: "AUTHENTIC",
+        highlightB: "SELF",
+        standardB: "WHILE GOING",
+        standardC: "FOR YOUR",
+        highlightC: "DREAM JOB",
+        description: "Find out what materials work best with your natural hair and check out some of our favorite brands"
+    }
+]
