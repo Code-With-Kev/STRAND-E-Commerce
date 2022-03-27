76ff79c0125ac9ecfef2fe768328fb1dd7d6a0b4
Code-With-Kev <96272858+Code-With-Kev@users.noreply.github.com>
2022-03-26 23:33:40 -0700

    Single Product Page Almost Complete

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -11,27 +11,27 @@ const ProductList = () => {
         <Announcement />
         {/* <h1 className="product-list--title">Hair Care</h1> */}
         <div className="filter">
-            <div className="filter--option">
-                <span className="filter--text">Filter Products:</span>         
-                <select className="filter--select">
-                    <option className="filter--type" disabled selected>Product Type</option>
-                    <option className="filter--type">Leave Ins</option>
-                    <option className="filter--type">Deep Conditioners</option>
-                    <option className="filter--type">Oils</option>
+            <div className="filter__option filter__option--product">
+                <span className="filter__text filter__text--black">Filter Products:</span>         
+                <select className="filter__select">
+                    <option className="filter__type" disabled selected>Product Type</option>
+                    <option className="filter__type">Leave Ins</option>
+                    <option className="filter__type">Deep Conditioners</option>
+                    <option className="filter__type">Oils</option>
                 </select>
-                <select className="filter--select">
-                    <option className="filter--type" disabled selected>Collection</option>
-                    <option className="filter--type">Moroccan Desert</option>
-                    <option className="filter--type">Manuka Honey</option>
-                    <option className="filter--type">Shea Serenity</option>
+                <select className="filter__select">
+                    <option className="filter__type" disabled selected>Collection</option>
+                    <option className="filter__type">Moroccan Desert</option>
+                    <option className="filter__type">Manuka Honey</option>
+                    <option className="filter__type">Shea Serenity</option>
                 </select>
             </div>
-            <div className="filter--option">
-                <span className="filter--text">Sort Products:</span>
-                <select className="filter--select">
-                    <option className="filter--type" selected>Newest</option>
-                    <option className="filter--type">Price (asc)</option>
-                    <option className="filter--type">Price (desc)</option>
+            <div className="filter__option filter__option--product">
+                <span className="filter__text filter__text--black">Sort Products:</span>
+                <select className="filter__select">
+                    <option className="filter__type" selected>Newest</option>
+                    <option className="filter__type">Price (asc)</option>
+                    <option className="filter__type">Price (desc)</option>
                     
                 </select> 
             </div>
--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -16,9 +16,42 @@ const SingleProduct = () => {
                 <img className="item__image" src="https://images.pexels.com/photos/9774855/pexels-photo-9774855.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="" />
             </div>
             <div className="item__info-container">
+                
                 <h1 className="item__title">Manuka Honey Deep Conditioner</h1>
                 <p className="item__description">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt nam quibusdam cupiditate sequi iste possimus suscipit, assumenda accusantium eligendi ipsam corrupti, velit, dolore delectus porro officia impedit. Itaque repudiandae nisi molestias, tempora maxime maiores nobis nihil eligendi, deserunt a aliquid.</p>
-                <div className="align-end"><span className="item__price">$234.99</span></div>
+
+                <div className="filter__option filter__option--single">
+                    <div className="filter__left">
+                        <span className="filter__text">Choose A Size:</span>
+                        <div className="filter__sizes">
+                            <div className="type--bg">
+                                <div className="filter__type">8 oz</div>
+                            </div>
+                            <div className="type--bg">
+                                <div className="filter__type">16 oz</div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="filter__right">
+                        <span className="item__price">$234.99</span>
+                    </div>
+                </div>
+
+                    <div className="quantity">
+                        <div className="quantity__amount">
+                            <div className="quantity__remove" />
+                            <span className="quantity__number">1</span>
+                            <div className="quantity__add" />
+                        </div>
+                        <div className="button-container">
+                            <button className="add-button">ADD TO CART</button>
+                        </div>
+                        </div>
+
+
+                <div className="divider" />
+            
             </div>
         </div>
 
