cdee050ca1afea8bf54fecb0605a37ea7017abe0
Code-With-Kev <96272858+Code-With-Kev@users.noreply.github.com>
2022-03-29 00:42:17 -0700

    Finished Setting Up Product Info

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -16,7 +16,29 @@ const Cart = () => {
                 <button className="add-button add-button--right">CHECKOUT NOW</button>
             </div>
             <div className="cart__bottom">
-                <span className="cart__info">INFO</span>
+                <span className="cart__info">
+                    <div className="cart__product">
+                        <div className="cart__product-detail">
+                            <img className="cart__detail--image" src="https://images.unsplash.com/photo-1608248597279-f99d160bfcbc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1712&q=80" alt="Product Image" />
+                            <div className="cart__detail--text-wrapper">
+                                <span className="cart__product--name">
+                                    <b>Product:</b> WILL SMITH OIL
+                                </span>
+                                <span className="cart__product--id">
+                                    <b>ID:</b> 57334748435
+                                </span>
+                                <span className="cart__product--size">
+                                    <b>Size:</b> 16oz
+                                </span>
+                            </div>
+                        </div>
+                        
+                        
+                        <div className="cart__price-detail">
+                            
+                        </div>
+                    </div> 
+                </span>
                 <span className="cart__summary">SUMMARY</span>
             </div>
         </div>
