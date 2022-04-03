473c486ed84626e131ce0d9ed64576debdfb4457
Code-With-Kev <96272858+Code-With-Kev@users.noreply.github.com>
2022-04-03 15:56:41 -0700

    Models complete

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const userRoute = require("./routes/User")
 
 dotenv.config();
 
@@ -11,7 +12,9 @@ mongoose.connect(process.env.MONGO_URL)
         console.log(err)
     });
 
+app.use(express.json());
+app.use("/api/users", userRoute);
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log("Backend server is running!")
 });
