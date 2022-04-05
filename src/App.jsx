e08ca01fa86c2ef8569605d4390ab3d6756e80fe
Code-With-Kev <96272858+Code-With-Kev@users.noreply.github.com>
2022-04-05 14:56:00 -0700

    Connecting front-end and backend

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,24 @@ import SingleProduct from "./pages/SingleProduct";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Cart from "./pages/Cart";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 const App = () => {
-    return <SingleProduct />
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/">
+                    <Home/>
+                </Route>
+                <Route exact path="/products">
+                    <ProductList/>
+                </Route>
+                <Route exact path="/product">
+                    <SingleProduct/>
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    );
 };
 
 export default App;
